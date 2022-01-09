164642489b9d7c74e4b4a1d1477213af37b6d549
bigtech-nerd <97368418+bigtech-nerd@users.noreply.github.com>
2022-01-08 20:21:53 -0500

    Create Password and secret.h

---
+++ examples/ESP8266/Password and secret.h
@@ -0,0 +1,7 @@
+String ssid = "name";       // AP NAME
+String pass = "password"; // AP PASSWORD
+String API = "key";   // Write API KEY
+WiFiClient  client; 
+
+https://forum.arduino.cc/t/arduino_secrets-h-no-such-file-or-directory/564140/4 
+(for secret problem)
